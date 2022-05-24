8df69eef11d0158c49ba46a4563fe0680f049f9a
Blair Bristow <blairhbristow@gmail.com>
2022-05-24 11:26:51 -0400

    Finish

---
+++ MyCodingSkills.java
@@ -0,0 +1,12 @@
+public class MyCodingSkills {
+    public static void main(String[] args) {
+        System.out.println("I understand HTML!");
+        System.out.println("I understand CSS!");
+        System.out.println("I understand JavaScript!");
+        System.out.println("I understand TypseScript!");
+        System.out.println("I understand Angular!");
+        System.out.println("I understand the Command Line!");
+        System.out.println("I understand Git!");
+        System.out.println("I understand(sorta) Java!");
+    }
+}
