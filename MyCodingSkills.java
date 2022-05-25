1b2268729e193dff0501bdb4d62f519bb58e7157
Blair Bristow <blairhbristow@gmail.com>
2022-05-25 12:31:56 -0400

    Implement iteration

--- MyCodingSkills.java
+++ MyCodingSkills.java
@@ -1,12 +1,10 @@
 public class MyCodingSkills {
     public static void main(String[] args) {
-        System.out.println("I understand HTML!");
-        System.out.println("I understand CSS!");
-        System.out.println("I understand JavaScript!");
-        System.out.println("I understand TypseScript!");
-        System.out.println("I understand Angular!");
-        System.out.println("I understand the Command Line!");
-        System.out.println("I understand Git!");
-        System.out.println("I understand(sorta) Java!");
+
+        String[] programmingLanguages = {"HTML", "CSS", "JavaScript", "TypeScript", "Angular", "Git", "GitHub", "Java"};
+
+        for (int i = 0; i < programmingLanguages.length; i++) {
+            System.out.println("I understand " + programmingLanguages[i]);
+        }
     }
 }
